fadc96c60e87a96ce5a5004a844f58b1d175a30a
MrYious <rosariomark37@gmail.com>
2024-02-10 19:09:52 +0800

    add body bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        {children}
+        <div className="bg-gray-200">{children}</div>
       </body>
     </html>
   );
